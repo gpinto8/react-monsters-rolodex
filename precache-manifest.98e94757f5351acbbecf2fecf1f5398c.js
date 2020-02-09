c54ff17291c7cd63db61f9775fcb6f2caabe55fb
GiuseppePinto09 <you@example.com>
2020-02-09 02:56:21 +0100

    Updates

---
+++ precache-manifest.98e94757f5351acbbecf2fecf1f5398c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed2a863af11583e887508669fa4ad7d4",
+    "url": "/monsters-rolodex.git/index.html"
+  },
+  {
+    "revision": "5e9b33f4418bb374d8a4",
+    "url": "/monsters-rolodex.git/static/css/main.43ce26a1.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex.git/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex.git/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e9b33f4418bb374d8a4",
+    "url": "/monsters-rolodex.git/static/js/main.6776f4fe.chunk.js"
+  },
+  {
+    "revision": "09912511e74398f94a37",
+    "url": "/monsters-rolodex.git/static/js/runtime-main.efae399d.js"
+  }
+]);
